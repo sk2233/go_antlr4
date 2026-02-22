c9f483a83af922e56e656076b0ab14c2fedc5a21
sky <2171783438@qq.com>
2026-02-22 22:42:24 +0800

    第一次提交

--- main.go
+++ main.go
@@ -3,31 +3,33 @@ package main
 import (
 	"ANTLR_test/ast"
 	"fmt"
+	"strings"
 
 	"github.com/antlr4-go/antlr/v4"
 )
 
 func main() {
 	RunCode("return 12*(1+2)+12/2;")
-	RunCode("var name=2233;" +
+	RunCode("var name=2233;\n" +
 		"return name;")
-	RunCode("var name=22;" +
-		"if(name==22){" +
-		"name=11;" +
-		"}" +
+	RunCode("var name=22;\n" +
+		"if(name==22){\n" +
+		"name=11;\n" +
+		"}\n" +
 		"return name;")
-	RunCode("var name=22;" +
-		"for(var i=0;i==0;i=i+1;){" +
-		"name=name+1;" +
-		"}" +
+	RunCode("var name=22;\n" +
+		"for(var i=0;i==0;i=i+1;){\n" +
+		"name=name+1;\n" +
+		"}\n" +
 		"return name;")
 	RunCode("var add=func(num1,num2){\n" +
 		"return num1+num2;\n" +
 		"};\n" +
-		"return add(1+2,2+3);\n")
+		"return add(1+2,2+3);")
 }
 
 func RunCode(code string) {
+	fmt.Println(strings.Split(code, "\n")[0], "...")
 	// 输入内容
 	input := antlr.NewInputStream(code)
 	// 创建词法分析器
