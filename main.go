968cf75a2d5cfd9b6c6bb8e639a5f9dd799d4d6e
shaokang.sk <shaokang@wepie.com>
2026-02-22 22:20:55 +0800

    实现新功能

--- main.go
+++ main.go
@@ -8,22 +8,22 @@ import (
 )
 
 func main() {
-	//RunCode("return 12*(1+2)+12/2")
-	//RunCode("var name=2233;" +
-	//	"return name;")
-	//RunCode("var name=22;" +
-	//	"if(name==22){" +
-	//	"name=11;" +
-	//	"}" +
-	//	"return name;")
-	//RunCode("var name=22;" +
-	//	"for(var i=0;i==0;i=i+1;){" +
-	//	"name=name+1;" +
-	//	"}" +
-	//	"return name;")
+	RunCode("return 12*(1+2)+12/2;")
+	RunCode("var name=2233;" +
+		"return name;")
+	RunCode("var name=22;" +
+		"if(name==22){" +
+		"name=11;" +
+		"}" +
+		"return name;")
+	RunCode("var name=22;" +
+		"for(var i=0;i==0;i=i+1;){" +
+		"name=name+1;" +
+		"}" +
+		"return name;")
 	RunCode("var add=func(num1,num2){\n" +
 		"return num1+num2;\n" +
-		"}\n" +
+		"};\n" +
 		"return add(1+2,2+3);\n")
 }
 
